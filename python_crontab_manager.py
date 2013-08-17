e69927b3dd3f5bef056ba0bf480467d11c95a7e4
el <wojciech@coders-mill.com>
2013-08-17 14:12:48 +0200

    Update python_crontab_manager.py

--- python_crontab_manager.py
+++ python_crontab_manager.py
@@ -5,7 +5,7 @@ class CronManager(object):
     """ 
     This is manager to create and remove jobs from crontab. It takes as
     arguments flask app - to get application root_path - and crontab as an 
-    instance of CronTab class from crontab package included in python-crontab
+    instance of CronTab class from crontab module included in python-crontab
     package.
 
     Usage:
